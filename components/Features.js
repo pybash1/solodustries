747a9560872e6f62f187f9c35cfde0deacf3dc4b
pybash1 <example@example.com>
2022-06-11 12:07:02 +0530

    fix(deployment): fix deployment errors

--- components/Features.js
+++ components/Features.js
@@ -1,23 +1,23 @@
 export default function Features() {
   return (
-    <section class="text-white bg-gray-900">
-      <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-        <div class="max-w-lg mx-auto text-center">
-          <h2 class="text-3xl font-bold sm:text-4xl">
+    <section className="text-white bg-gray-900">
+      <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <div className="max-w-lg mx-auto text-center">
+          <h2 className="text-3xl font-bold sm:text-4xl">
             Kickstart your Freelancing Career
           </h2>
 
-          <p class="mt-4 text-gray-300">
+          <p className="mt-4 text-gray-300">
             Wanted to freelance, but couldn't because of these limitations? Well
             don't worry anymore, cause we have got you covered!
           </p>
         </div>
 
-        <div class="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
-          <a class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
+        <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
+          <a className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-10 h-10 text-indigo-500"
+              className="w-10 h-10 text-indigo-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -25,26 +25,26 @@ export default function Features() {
               <path d="M12 14l9-5-9-5-9 5 9 5z" />
               <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
               />
             </svg>
 
-            <h3 class="mt-4 text-xl font-bold text-white">
+            <h3 className="mt-4 text-xl font-bold text-white">
               Income and Expense Management
             </h3>
 
-            <p class="mt-1 text-sm text-gray-300">
+            <p className="mt-1 text-sm text-gray-300">
               Having trouble managing your money and expenses? Don't want to use another external app? Don't worry we have you covered!
             </p>
           </a>
 
-          <a class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
+          <a className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-10 h-10 text-indigo-500"
+              className="w-10 h-10 text-indigo-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -52,24 +52,24 @@ export default function Features() {
               <path d="M12 14l9-5-9-5-9 5 9 5z" />
               <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
               />
             </svg>
 
-            <h3 class="mt-4 text-xl font-bold text-white">Client Management</h3>
+            <h3 className="mt-4 text-xl font-bold text-white">Client Management</h3>
 
-            <p class="mt-1 text-sm text-gray-300">
+            <p className="mt-1 text-sm text-gray-300">
               Overwhelmed with clients, ratings, reviews, information? We all are! Using our smart client management technology, you can save and find clients whenever and wherever you need!
             </p>
           </a>
 
-          <a class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
+          <a className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-10 h-10 text-indigo-500"
+              className="w-10 h-10 text-indigo-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -77,29 +77,29 @@ export default function Features() {
               <path d="M12 14l9-5-9-5-9 5 9 5z" />
               <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
               />
             </svg>
 
-            <h3 class="mt-4 text-xl font-bold text-white">
+            <h3 className="mt-4 text-xl font-bold text-white">
               Scheduling and Calendar &nbsp;
-              <strong class="bg-indigo-500 border-indigo-500 border text-white px-3 py-1.5 rounded text-[12px] font-medium">
+              <strong className="bg-indigo-500 border-indigo-500 border text-white px-3 py-1.5 rounded text-[12px] font-medium">
                 New
               </strong>
             </h3>
 
-            <p class="mt-1 text-sm text-gray-300">
+            <p className="mt-1 text-sm text-gray-300">
               Now you can integrate Google Calendar with Solodustries to manage your schedule as well!
             </p>
           </a>
 
-          <a class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
+          <a className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-10 h-10 text-indigo-500"
+              className="w-10 h-10 text-indigo-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -107,24 +107,24 @@ export default function Features() {
               <path d="M12 14l9-5-9-5-9 5 9 5z" />
               <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
               />
             </svg>
 
-            <h3 class="mt-4 text-xl font-bold text-white">Track Projects</h3>
+            <h3 className="mt-4 text-xl font-bold text-white">Track Projects</h3>
 
-            <p class="mt-1 text-sm text-gray-300">
+            <p className="mt-1 text-sm text-gray-300">
               Having a lot of clients, means a lot of projects and it can get really hard to manage all of them on time! Not anymore, with our amazing project trakcing and management solution!
             </p>
           </a>
 
-          <a class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
+          <a className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-10 h-10 text-indigo-500"
+              className="w-10 h-10 text-indigo-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -132,24 +132,24 @@ export default function Features() {
               <path d="M12 14l9-5-9-5-9 5 9 5z" />
               <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
               />
             </svg>
 
-            <h3 class="mt-4 text-xl font-bold text-white">Manage Resources</h3>
+            <h3 className="mt-4 text-xl font-bold text-white">Manage Resources</h3>
 
-            <p class="mt-1 text-sm text-gray-300">
+            <p className="mt-1 text-sm text-gray-300">
               As a freelancer, we often find a relly nice resource, but not saving it causes it to get lost forever! But with Solodustries, never forget one again! Just one click, and its saved!
             </p>
           </a>
 
-          <a class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
+          <a className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-indigo-500/10 hover:border-indigo-500/10">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-10 h-10 text-indigo-500"
+              className="w-10 h-10 text-indigo-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -157,39 +157,39 @@ export default function Features() {
               <path d="M12 14l9-5-9-5-9 5 9 5z" />
               <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
               />
             </svg>
 
-            <h3 class="mt-4 text-xl font-bold text-white">Promote Yourself</h3>
+            <h3 className="mt-4 text-xl font-bold text-white">Promote Yourself</h3>
 
-            <p class="mt-1 text-sm text-gray-300">
+            <p className="mt-1 text-sm text-gray-300">
               Promote Yourself on our website, so you can find better clients, faster!
             </p>
           </a>
         </div>
 
-        <div class="mt-12 text-center">
+        <div className="mt-12 text-center">
           <a
-            class="inline-flex items-center px-8 py-3 mt-8 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent active:text-indigo-500 focus:outline-none focus:ring"
+            className="inline-flex items-center px-8 py-3 mt-8 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent active:text-indigo-500 focus:outline-none focus:ring"
             href="/get-started"
           >
-            <span class="text-sm font-medium"> Get Started </span>
+            <span className="text-sm font-medium"> Get Started </span>
 
             <svg
-              class="w-5 h-5 ml-3"
+              className="w-5 h-5 ml-3"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M17 8l4 4m0 0l-4 4m4-4H3"
               />
             </svg>
--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,18 @@
 export default function Footer() {
   return (
-    <footer class="text-center bg-gray-900 text-white">
-      <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
-        <div class="max-w-3xl mx-auto space-y-6">
-          <div class="flex justify-center space-x-6">
+    <footer className="text-center bg-gray-900 text-white">
+      <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto space-y-6">
+          <div className="flex justify-center space-x-6">
             <a
-              class="text-blue-500 hover:text-opacity-75"
+              className="text-blue-500 hover:text-opacity-75"
               href="https://twitter.com/py_bash1"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Twitter"
             >
               <svg
-                class="w-8 h-8"
+                className="w-8 h-8"
                 fill="currentColor"
                 viewBox="0 0 24 24"
                 aria-hidden="true"
@@ -22,20 +22,20 @@ export default function Footer() {
             </a>
 
             <a
-              class="text-white hover:text-opacity-75"
+              className="text-white hover:text-opacity-75"
               href="https://github.com/pybash1/solodustries"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="GitHub"
             >
               <svg
-                class="w-8 h-8"
+                className="w-8 h-8"
                 fill="currentColor"
                 viewBox="0 0 24 24"
                 aria-hidden="true"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
                   clip-rule="evenodd"
                 ></path>
@@ -43,9 +43,9 @@ export default function Footer() {
             </a>
           </div>
 
-          <nav class="relative flex flex-wrap justify-center gap-8 p-8 text-sm font-bold border-4 border-white rounded-xl">
+          <nav className="relative flex flex-wrap justify-center gap-8 p-8 text-sm font-bold border-4 border-white rounded-xl">
             <a
-              class="hover:opacity-75"
+              className="hover:opacity-75"
               href="/"
               target="_blank"
               rel="noopener noreferrer"
@@ -54,7 +54,7 @@ export default function Footer() {
             </a>
 
             <a
-              class="hover:opacity-75"
+              className="hover:opacity-75"
               href="https://github.com/pybash1"
               target="_blank"
               rel="noopener noreferrer"
@@ -63,7 +63,7 @@ export default function Footer() {
             </a>
 
             <a
-              class="hover:opacity-75"
+              className="hover:opacity-75"
               href="https://pybash.hashnode.dev"
               target="_blank"
               rel="noopener noreferrer"
@@ -72,7 +72,7 @@ export default function Footer() {
             </a>
 
             <a
-              class="hover:opacity-75"
+              className="hover:opacity-75"
               href="https://pybash.is-a.dev"
               target="_blank"
               rel="noopener noreferrer"
@@ -81,10 +81,10 @@ export default function Footer() {
             </a>
           </nav>
 
-          <p class="max-w-lg mx-auto text-xs text-gray-500">
+          <p className="max-w-lg mx-auto text-xs text-gray-500">
             Solodustries is a completely free to use and open source software, with its source available on GitHub. It was made as a project for the OneHacks II Hackathon by PyBash.
             <a
-              class="block mt-1 text-indigo-600 underline decoration-wavy decoration-indigo-500 hover:opacity-75"
+              className="block mt-1 text-indigo-600 underline decoration-wavy decoration-indigo-500 hover:opacity-75"
               href="/"
               target="_blank"
               rel="noopener noreferrer"
@@ -92,7 +92,7 @@ export default function Footer() {
               OneHacks II Hackathon
             </a>
             <a
-              class="block mt-1 text-indigo-600 underline decoration-wavy decoration-indigo-500 hover:opacity-75"
+              className="block mt-1 text-indigo-600 underline decoration-wavy decoration-indigo-500 hover:opacity-75"
               href="/"
               target="_blank"
               rel="noopener noreferrer"
@@ -101,7 +101,7 @@ export default function Footer() {
             </a>
           </p>
 
-          <p class="text-xs font-medium">2022 Py Bash</p>
+          <p className="text-xs font-medium">2022 Py Bash</p>
         </div>
       </div>
     </footer>
